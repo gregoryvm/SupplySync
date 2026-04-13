62c456a1b8c88b1a2527536ef1156c5f6fd633d1
gregoryv <gregoryv@myumanitoba.ca>
2026-04-13 15:35:28 -0500

    product model setup

--- supplysync_backend/supplysync_app/models.py
+++ supplysync_backend/supplysync_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField()
+    quantity = models.IntegerField()
+    sku = models.CharField()
+    weight = models.FloatField()
+    cost =  models.FloatField()
+    price =  models.FloatField()
+    category = models.CharField()
+
+    def __str__(self):
+        return self.name
